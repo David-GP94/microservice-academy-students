4ffb6fbcb8543613ed1e4ac8f84c4a751663ce42
PROCHURGRUPOCSI\david.garcia <david.garcia@grupocsi.com>
2025-08-05 23:13:50 -0600

    First commit students

---
+++ src/main/java/com/academy/microservice_academy_students/MicroserviceAcademyStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.academy.microservice_academy_students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAcademyStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAcademyStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/microservice_academy_students/MicroserviceAcademyStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.microservice_academy_students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAcademyStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
